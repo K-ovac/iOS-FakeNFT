1d3c65ada4b7cce9b485b787afc0338fc843a10e
Kovac <biu.jay1@yandex.ru>
2026-03-20 23:25:05 +0300

    chore: убран сториборд, настроен TabBarController программно

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -2,24 +2,64 @@ import UIKit
 
 final class TabBarController: UITabBarController {
 
-    var servicesAssembly: ServicesAssembly!
+    var servicesAssembly: ServicesAssembly
 
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
+    private let profileTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.profile", comment: ""),
+        image: UIImage(systemName: "person.crop.circle.fill"),
         tag: 0
     )
-
+    
+    private let catalogTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.catalog", comment: ""),
+        image: UIImage(named: "catalogTabBarItem"),
+        tag: 1
+    )
+    
+    private let cartTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.cart", comment: ""),
+        image: UIImage(named: "cartTabBarItem"),
+        tag: 2
+    )
+    
+    private let statisticsTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.statistics", comment: ""),
+        image: UIImage(systemName: "flag.2.crossed.fill"),
+        tag: 3
+    )
+    
+    init(servicesAssembly: ServicesAssembly) {
+            self.servicesAssembly = servicesAssembly
+            super.init(nibName: nil, bundle: nil)
+        }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    private func configureTabBar() {
+        view.backgroundColor = UIColor.background
+        tabBar.unselectedItemTintColor = .black
+        
+        let profileController = UIViewController()  //изменить на свой
+        profileController.tabBarItem = profileTabBarItem
 
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
-        )
+        )   //изменить на свой
         catalogController.tabBarItem = catalogTabBarItem
+        
+        let cartController = UIViewController() //изменить на свой
+        cartController.tabBarItem = cartTabBarItem
+        
+        let statisticsController = UIViewController()   //изменить на свой
+        statisticsController.tabBarItem = statisticsTabBarItem
 
-        viewControllers = [catalogController]
-
-        view.backgroundColor = .systemBackground
+        viewControllers = [profileController, catalogController, cartController, statisticsController]
     }
 }
